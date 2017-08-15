85b251996115dccaba0040c5ab869bc1b030ba47
SobrinhoDeUlisses <brunosales@eng.ci.ufpb.br>
2017-08-15 08:28:13 -0300

    hello

---
+++ R02Q06.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+ char n[9],c[9];
+ int i, t=1;
+ n[8]=n[7]='a'
+
+ for(i=0;i<9;i++){
+   puts("digite os numero");
+   scanf("%c", &n[i]);
+ }
+ for(i=0;i<9;i++){
+   if(n[i]=='-'){
+   t=0;
+
+    if(n[8]=='a'){
+
+      for(i=0;i<9;i++){
+       n[i]=c[i+1];
+       c[0]=3;
+   }
+
+ }
+ }
+ }
+ if(t){
+if()n[7]=='a'){
+  for(i=0;i<8;i++){
+   n[i]=c[i+1];
+   c[0]=3;
+  }
+
+}
+}
+
+return 0;
+
+}
